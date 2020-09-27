92d265a80cdd15ef91dfe50152acfd427e462b37
marsha <marshaannon@gmail.com>
2020-09-26 22:19:14 -0400

    add PUT request

--- S3V5_edit_quote/app.js
+++ S3V5_edit_quote/app.js
@@ -16,11 +16,10 @@ app.get('/quotes/:id', async (req, res)=>{
         const quote = await records.getQuote(req.params.id);
         if(quote){
             res.json(quote);
-        } else {
+        }else {
             res.status(404).json({message: "Quote not found."});
-        }
-        
-    } catch(err) {
+        }   
+    }catch(err) {
         res.status(500).json({message: err.message});
     }
 });
@@ -34,15 +33,30 @@ app.post('/quotes', async (req,res) =>{
                 author: req.body.author
             });
             res.status(201).json(quote);
-        } else {
+        }else {
             res.status(400).json({message: "Quote and author required."});
         }
-
-    } catch(err) {
+    }catch(err) {
         res.status(500).json({message: err.message});
     } 
 });
 // Send a PUT request to /quotes/:id to UPDATE (edit) a quote
+app.put('/quotes/:id', async (req, res) => {
+    try {
+        const quote = await records.getQuote(req.params.id);
+        if(quote) {
+            quote.quote = req.body.quote;
+            quote.author = req.body.author;
+
+            await records.updateQuote(quote);
+            res.status(204).end();
+        }else {
+            res.status(404).json({message: 'Quote Not Found'})
+        }
+    }catch(err) {
+        res.status(500).json({message: err.message});
+    }
+});
 // Send a DELETE request to /quotes/:id DELETE a quote 
 // Send a GET request to /quotes/quote/random to READ (view) a random quote
 
